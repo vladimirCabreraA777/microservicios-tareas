93a3b758b8c9cdaecb17ce695c1dd5ae13683ddc
Vladimir Cabrera Arandia <cabreravladimir@gmail.com>
2026-04-27 16:36:01 -0400

    Update Program.cs

--- api-gateway/Program.cs
+++ api-gateway/Program.cs
@@ -1,1 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient();
+var app = builder.Build();
 
+app.MapGet("/", () => "API Gateway de Microservicios IA");
+app.MapGet("/tasks", async (IHttpClientFactory clientFactory) =>
+{
+    var client = clientFactory.CreateClient();
+    var response = await client.GetStringAsync("http://task-service/tasks");
+    return Results.Ok(response);
+});
+
+app.Run();
